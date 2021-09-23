ea7827a667bea7f8816031d5c8d62273c801c4d9
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-09-23 13:59:23 +0200

    refactor

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn transpose<'a>(notes: &'a [&str], note: &str, semitones: i32) -> &'a str {
+    let mut pos = notes.iter().position(|&x| x == note).unwrap() as i32;
+    pos += semitones;
+    let new_note = notes
+        .get(pos as usize)
+        .expect("cannot transpose: exceeded bounds");
+    new_note
+}
+
+fn readfile(filename: &str) -> Vec<String> {
+    let contents;
+    match fs::read_to_string(filename) {
+        Ok(s) => contents = s,
+        Err(_) => {
+            eprintln!("could not read file");
+            panic!();
+        }
+    }
+    let contents: Vec<String> = contents.split_whitespace().map(|s| s.to_string()).collect();
+    contents
+}
+
+pub fn run(filename: &str, semitones: i32) {
+    let notes = [
+        "1", "-1'", "-1", "1o", "2", "-2''", "-2'", "-2", "-3'''", "-3''", "-3'", "-3", "4", "-4'",
+        "-4", "4o", "5", "-5", "5o", "6", "-6'", "-6", "6o", "-7", "7", "-7o", "-8", "8'", "8",
+        "-9", "9'", "9", "-9o", "-10", "10''", "10'", "10",
+    ];
+
+    let contents = readfile(filename);
+
+    for note in contents {
+        let new_note = transpose(&notes, note.as_str(), semitones);
+        print!("{} ", new_note);
+    }
+    println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 use clap::{App, Arg};
-use std::fs;
-
-fn transpose<'a>(notes: &'a [&str], note: &str, semitones: i32) -> &'a str {
-    let mut pos = notes.iter().position(|&x| x == note).unwrap() as i32;
-    pos += semitones;
-    let new_note = notes
-        .get(pos as usize)
-        .expect("cannot transpose: exceeded bounds");
-    new_note
-}
+use harptabs::run;
 
 fn main() {
     let matches = App::new("harptool")
@@ -23,34 +14,13 @@ fn main() {
         .arg(
             Arg::with_name("file")
                 .value_name("FILE")
-                .help("file containing tabs")
+                .help("file containing tabs"),
         )
         .get_matches();
 
     let filename = matches.value_of("file").unwrap_or("tabs.txt");
-    let contents;
-    match fs::read_to_string(filename) {
-        Ok(s) => contents = s,
-        Err(_) => {
-            eprintln!("could not read file");
-            panic!();
-        }
-    }
-    let contents: Vec<&str> = contents.split_whitespace()
-        .collect();
-
     let semitones = matches.value_of("semitones").unwrap_or("0");
     let semitones = semitones.parse::<i32>().unwrap();
 
-    let notes = [
-        "1", "-1'", "-1", "1o", "2", "-2''", "-2'", "-2", "-3'''", "-3''", "-3'", "-3", "4", "-4'",
-        "-4", "4o", "5", "-5", "5o", "6", "-6'", "-6", "6o", "-7", "7", "-7o", "-8", "8'", "8",
-        "-9", "9'", "9", "-9o", "-10", "10''", "10'", "10",
-    ];
-
-    for note in contents {
-        let new_note = transpose(&notes, note, semitones);
-        print!("{} ", new_note);
-    }
-    println!();
+    run(filename, semitones);
 }
