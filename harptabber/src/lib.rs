c3bbff7d98e65d4a1f0fd14d9397fbd51e994e62
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-09-24 11:55:54 +0200

    refactor

--- harptabber/src/lib.rs
+++ harptabber/src/lib.rs
@@ -18,71 +18,58 @@ fn transpose<'a>(notes: &'a [&str], note: &str, semitones: i32) -> Result<&'a st
     }
 }
 
-fn transpose_position<'a>(
-    notes: &'a [&str],
-    note: &str,
-    from_position: i32,
-    to_position: i32,
-    octave_shift: i32,
-) -> Result<&'a str, String> {
+fn positions_to_semitones(from_position: i32, to_position: i32, octave_shift: i32) -> i32 {
     let diff = to_position - from_position;
     let semitones = ((diff * 7) % 12) + 12 * octave_shift;
-    transpose(notes, note, semitones)
-}
-
-fn readfile(filename: &str) -> Vec<Vec<String>> {
-    let contents;
-    match fs::read_to_string(filename) {
-        Ok(s) => contents = s,
-        Err(_) => {
-            eprintln!("could not read file");
-            std::process::exit(-1);
-        }
-    }
-    let mut lines: Vec<Vec<String>> = Vec::new();
-    for line in contents.lines() {
-        let line: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
-        lines.push(line);
-    }
-    lines
+    semitones
 }
 
 pub fn run(
     filename: &str,
-    semitones: i32,
+    mut semitones: i32,
     from_position: i32,
     to_position: Option<i32>,
     octave_shift: i32,
     no_error: bool,
 ) {
+    let tab = match fs::read_to_string(filename) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("could not read file");
+            std::process::exit(-1);
+        }
+    };
+
+    if let Some(to_position) = to_position {
+        semitones = positions_to_semitones(from_position, to_position, octave_shift);
+    } else if octave_shift != 0 {
+        semitones = positions_to_semitones(from_position, 1, octave_shift);
+    }
+
+    let tabs = transpose_tabs(tab, semitones, no_error);
+    print!("{}", tabs);
+}
+
+fn transpose_tabs(tab: String, semitones: i32, no_error: bool) -> String {
     let notes = [
         "1", "-1'", "-1", "1o", "2", "-2''", "-2'", "-2", "-3'''", "-3''", "-3'", "-3", "4", "-4'",
         "-4", "4o", "5", "-5", "5o", "6", "-6'", "-6", "6o", "-7", "7", "-7o", "-8", "8'", "8",
         "-9", "9'", "9", "-9o", "-10", "10''", "10'", "10", "-10o",
     ];
 
-    let contents = readfile(filename);
+    let mut result = String::from("");
 
-    for line in contents {
+    for line in tab.lines() {
+        let line: Vec<&str> = line.split_whitespace().collect();
+
         for note in line {
-            let new_note;
-            if let Some(to_position) = to_position {
-                new_note = transpose_position(
-                    &notes,
-                    note.as_str(),
-                    from_position,
-                    to_position,
-                    octave_shift,
-                );
-            } else if octave_shift != 0 {
-                new_note =
-                    transpose_position(&notes, note.as_str(), from_position, 1, octave_shift);
-            } else {
-                new_note = transpose(&notes, note.as_str(), semitones);
-            }
+            let new_note = transpose(&notes, note, semitones);
 
             match new_note {
-                Ok(new_note) => print!("{} ", new_note),
+                Ok(new_note) => {
+                    result.push_str(new_note);
+                    result.push_str(" ");
+                }
                 Err(s) => {
                     if !no_error {
                         eprintln!("{}", s);
@@ -91,6 +78,7 @@ pub fn run(
                 }
             }
         }
-        println!();
+        result.push_str("\n");
     }
+    result
 }
