f44615414a9774c63fb484329d4494e0435860e8
Michael Ebert-Hanke <mail@ebert-hanke.de>
2021-10-16 01:56:47 +0200

    disabled not allow warnings for build

--- harptabber-gui/src/lib.rs
+++ harptabber-gui/src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+//#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
