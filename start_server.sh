146a14a284a3cc760ec3242f38b77ef6e0c6284b
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-10-24 23:26:34 +0200

    skip installing basic-http-server in start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,8 +4,6 @@ set -eu
 # Starts a local web-server that serves the contents of the `doc/` folder,
 # which is the folder to where the web version is compiled.
 
-cargo install basic-http-server
-
 echo "open http://localhost:8080"
 xdg-open http://localhost:8080/index.html
 
