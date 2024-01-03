6a0101573965c7b52207d6bf684e77418f8487a0
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2024-01-03 23:50:52 +0100

    syntax highlight: fix dark mode

--- harptabber-gui/src/syntax_highlight.rs
+++ harptabber-gui/src/syntax_highlight.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeSet;
 
-use eframe::{egui, epaint::Color32};
+use eframe::egui;
 
 #[derive(Default)]
 pub struct MemoizedHighlighter {
@@ -17,7 +17,9 @@ impl MemoizedHighlighter {
         text: &str,
         invalid_notes: &[String],
     ) -> egui::text::LayoutJob {
-        if (&self.style, self.text.as_str(), self.errors.as_slice()) != (egui_style, text, invalid_notes) {
+        if (&self.style, self.text.as_str(), self.errors.as_slice())
+            != (egui_style, text, invalid_notes)
+        {
             self.style = egui_style.clone();
             self.text = text.to_owned();
             self.errors = invalid_notes.to_vec();
@@ -29,35 +31,38 @@ impl MemoizedHighlighter {
 
 fn highlight_tab(
     egui_style: &egui::Style,
-    mut text: &str,
+    text: &str,
     invalid_notes: &[String],
 ) -> egui::text::LayoutJob {
     dbg!(invalid_notes);
     let mut job = egui::text::LayoutJob::default();
 
-    let mut prev_pos = 0;
-    // let matches = [(3usize, 8usize), (10, 18)];
-
-    let mut indices: BTreeSet<(usize, usize)> = invalid_notes.iter().map(|invalid_note| {
-        text
-            .match_indices(invalid_note)
-            .map(|(i, string)| (i, i + string.len()))
-    }).flatten().collect();
+    // TODO use regex instead to match word boundary
+    let mut indices: BTreeSet<(usize, usize)> = invalid_notes
+        .iter()
+        .flat_map(|invalid_note| {
+            text.match_indices(invalid_note)
+                .map(|(i, string)| (i, i + string.len()))
+        })
+        .collect();
     dbg!(&indices);
 
-    
     let indices_copy = indices.clone();
-    indices.retain(|(start, stop)| !indices_copy.iter().any(|(other_start, other_stop)| {
-        (start >= other_start && stop <= other_stop) && !(start == other_start && stop == other_stop)
-    }));
+    indices.retain(|(start, stop)| {
+        !indices_copy.iter().any(|(other_start, other_stop)| {
+            (start >= other_start && stop <= other_stop)
+                && !(start == other_start && stop == other_stop)
+        })
+    });
 
-    if text.len() > 19 {
+    let mut prev_pos = 0;
+    if !indices.is_empty() {
         for &(start, stop) in indices.iter() {
             job.append(
                 &text[prev_pos..start],
                 0.0,
                 egui::text::TextFormat {
-                    color: Color32::WHITE,
+                    color: egui_style.visuals.text_color(),
                     ..Default::default()
                 },
             );
@@ -66,7 +71,7 @@ fn highlight_tab(
                 &text[start..stop],
                 0.0,
                 egui::text::TextFormat {
-                    color: Color32::RED,
+                    color: egui_style.visuals.error_fg_color,
                     ..Default::default()
                 },
             );
@@ -76,16 +81,16 @@ fn highlight_tab(
             &text[prev_pos..],
             0.0,
             egui::text::TextFormat {
-                color: Color32::WHITE,
+                color: egui_style.visuals.text_color(),
                 ..Default::default()
             },
         );
     } else {
         job.append(
-            &text[..],
+            text,
             0.0,
             egui::text::TextFormat {
-                color: Color32::WHITE,
+                color: egui_style.visuals.text_color(),
                 ..Default::default()
             },
         );
